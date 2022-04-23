ba366ba0b8d9a7d17f3df422076867b129744639
Manhlinh25 <97250874+Manhlinh25@users.noreply.github.com>
2022-04-23 19:34:36 +0700

    init code
    init code

---
+++ lookforapple/src/main/java/com/mmo/lookforapple/LookforappleApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforapple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforappleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforappleApplication.class, args);
+	}
+
+}
---
+++ lookforapple/src/test/java/com/mmo/lookforapple/LookforappleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforapple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforappleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
